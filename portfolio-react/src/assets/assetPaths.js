b27b6ccd0b827fa9996d330c675a77e844b0e5c8
sqln-ctrl <sqlnnqvi@gmail.com>
2026-07-16 01:26:39 +0500

    fix paths

--- portfolio-react/src/assets/assetPaths.js
+++ portfolio-react/src/assets/assetPaths.js
@@ -1,8 +1,8 @@
 
 
 export const assets = {
-  profilePic: `../public/assets/profilepic.png`,
-  resumePdf: `../public/assets/Saqlain_Naqvi_resume.pdf`,
+  profilePic: `../assets/profilepic.png`,
+  resumePdf: `../assets/Saqlain_Naqvi_resume.pdf`,
 };
 
 export default assets;
